f2cb4e93ccd729f824c66fd082a3230f17ed053b
John Lloyd <johnl5124@gmail.com>
2022-02-11 01:15:48 +0000

    Added RPiCam Module
    Implemented RPI cam module to take photos, just got to check BOOFcv works on this next...

--- src/main/java/com/ACORobotics/AlphaACORobotics.java
+++ src/main/java/com/ACORobotics/AlphaACORobotics.java
@@ -8,15 +8,17 @@ public class AlphaACORobotics
 	public static void main(String[] args) throws InterruptedException 
 	{		
 		System.out.println("Test of Movement, Camera and Ultrasound");
-		//CamTest();
+
+		RPiCam.takePhoto();
+		//RPiCam.qrScan();
 		
-		RobotMovement movementObj = new RobotMovement();
-		Thread movementThread = new Thread(movementObj);
-		movementThread.start();
-		
-		UltrasoundTest ultrasoundObj = new UltrasoundTest();
-		Thread ultrasoundThread = new Thread(ultrasoundObj);
-		ultrasoundThread.start();
+//		RobotMovement movementObj = new RobotMovement();
+//		Thread movementThread = new Thread(movementObj);
+//		movementThread.start();
+//		
+//		UltrasoundTest ultrasoundObj = new UltrasoundTest();
+//		Thread ultrasoundThread = new Thread(ultrasoundObj);
+//		ultrasoundThread.start();
 		
 //		UltrasoundTest test1 = new UltrasoundTest();
 //		
